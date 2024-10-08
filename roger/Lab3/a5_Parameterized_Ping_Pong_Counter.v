7cbb493433ff0fb6ce97299b272bc126f8987c2a
Roger Fan <sweric2u@gmail.com>
2024-10-08 21:02:09 +0800

    Roger: Lab3 a5, a5_t(Johnny)

--- roger/Lab3/a5_Parameterized_Ping_Pong_Counter.v
+++ roger/Lab3/a5_Parameterized_Ping_Pong_Counter.v
@@ -1,12 +1,32 @@
 `timescale 1ns/1ps
 
 module Parameterized_Ping_Pong_Counter (clk, rst_n, enable, flip, max, min, direction, out);
-input clk, rst_n;
-input enable;
-input flip;
-input [4-1:0] max;
-input [4-1:0] min;
-output direction;
-output [4-1:0] out;
+    input clk, rst_n;
+    input enable;
+    input flip;
+    input [4-1:0] max;
+    input [4-1:0] min;
+    output direction;
+    output [4-1:0] out;
+    reg direction;
+    reg [4-1:0] out;
 
+    reg next_direction;
+    always @(*) begin
+        if (out==min)       next_direction = 1'b1;
+        else if (out==max)  next_direction = 1'b0;
+        else if (flip)      next_direction = !direction;
+        else                next_direction = direction;
+    end
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            out <= min;
+            direction <= 1'b1;
+        end else if (enable && max >= out && out >= min && max != min) begin
+            direction <= next_direction;
+            if (next_direction) out <= out + 4'b1;
+            else                out <= out - 4'b1;
+        end
+    end
 endmodule
---
+++ roger/Lab3/a5_Parameterized_Ping_Pong_Counter_t.v
@@ -0,0 +1,79 @@
+`include "a5_Parameterized_Ping_Pong_Counter.v"
+`timescale 1ns/1ps
+
+module Parameterized_Ping_Pong_Counter_t();
+    reg clk = 0, rst_n = 0, enable = 1, flip = 0;
+    reg [3:0] max = 4'd4, min = 4'd0;
+    wire direction;
+    wire [3:0] out;
+    Parameterized_Ping_Pong_Counter PPPC(clk, rst_n, enable, flip, max, min, direction, out);
+
+    always #5 clk = !clk;
+    
+    initial begin
+        #150 flip = 1;
+        #10 flip = 0;
+    end
+
+    initial begin
+        #260 flip = 1;
+        #10 flip = 0;
+    end
+
+    initial begin
+        #280 flip = 1;
+        #10 flip = 0;
+    end
+
+    initial begin
+        #390 flip = 1;
+        #10 flip = 0;
+    end
+    
+    initial begin
+        #430 flip = 1;
+        #10 flip = 0;
+    end
+
+    initial begin
+        #470 flip = 1;
+        #10 flip = 0;
+    end
+
+    initial begin
+        #460 enable = 0;
+        #30 enable = 1;
+    end
+
+    initial begin
+        #10 rst_n = 1;
+        #90 rst_n = 0;
+        #20 rst_n = 1;
+        #90 rst_n = 0;
+        #20 rst_n = 1;
+        #90 rst_n = 0;
+        #20 rst_n = 1;
+    end
+
+    initial begin
+        #340 max = 3;
+        #80 max = 2;
+        #30 max = 3;
+    end
+
+    initial begin
+        #390 min = 1;
+        #10 min = 3;
+        #20 min = 2;
+        #30 min = 1;
+    end
+
+    initial begin
+        #515 $finish;
+    end
+
+    initial begin
+        $dumpfile("a5_Parameterized_Ping_Pong_Counter_t.vcd");
+        $dumpvars(0, Parameterized_Ping_Pong_Counter_t);
+    end
+endmodule
