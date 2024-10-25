053501786dc4b2c0a6c89c51b6251974fe3e733f
Roger Fan <sweric2u@gmail.com>
2024-10-25 21:37:42 +0800

    Roger/Final: fix

--- roger/Lab4/a4_Mealy_Sequence_Detector.v
+++ roger/Lab4/a4_Mealy_Sequence_Detector.v
@@ -5,9 +5,9 @@ module Mealy_Sequence_Detector (clk, rst_n, in, dec);
     input in;
     output dec;
 
+    reg [3-1:0] MEM;
     wire [4-1:0] num;
     assign num = {MEM, in};
-    reg [3-1:0] MEM;
     reg [2-1:0] cnt;
 
     assign dec = ((cnt==2'd3)&&((num==4'b0111)||(num==4'b1001)||(num==4'b1110)));
