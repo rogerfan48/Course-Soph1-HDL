ec3ffbabc78fd9a9a5f63de689b221eaa2b7fc76
coolpeko <johnny66634345@example.com>
2024-11-07 23:19:37 +0800

    Final/Johnny: Q4 debug

--- johnny/Lab5/Lab5_Team1_Booth_Multiplier_4bit_t.v
+++ johnny/Lab5/Lab5_Team1_Booth_Multiplier_4bit_t.v
@@ -1,4 +1,3 @@
-`include "Lab5_Team1_Booth_Multiplier_4bit.v"
 `timescale 1ns/1ps
 
 module Booth_Multiplier_4bit_t();
@@ -16,23 +15,68 @@ module Booth_Multiplier_4bit_t();
 
     initial begin
         #10
-        a = 4'b1000;
-        b = 4'b1000;
+        a = 4'd7;
+        b = 4'd0;
         #50
         $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
         #20
-        a = 4'b1000;
-        b = 4'd2;
+        a = 4'd0;
+        b = 4'd7;
         #50
         $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
         #20
-        a = 4'b1000;
+        a = 4'd0;
         b = 4'd7;
         #50
         $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
         #20
         a = 4'd7;
         b = 4'd7;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'b1000;
+        b = 4'd0;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'd0;
+        b = 4'b1000;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'd5;
+        b = 4'b1000;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'd4;
+        b = 4'b1000;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'b1011;
+        b = 4'd7;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'b1100;
+        b = 4'b1100;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'b1101;
+        b = 4'd0;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'b1001;
+        b = 4'd7;
+        #50
+        $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
+        #20
+        a = 4'd7;
+        b = 4'd5;
         #50
         $display("a = ", $signed(a), ", b = ", $signed(b), ", p = ", $signed(p), "\n");
         #20
@@ -52,10 +96,19 @@ module Booth_Multiplier_4bit_t();
         #10 start = 0;
         #60 start = 1;
         #10 start = 0;
-    end
-
-    initial begin
-        $dumpfile("Booth_Multiplier_4bit_t.vcd");
-        $dumpvars(0, Booth_Multiplier_4bit_t);
+        #60 start = 1;
+        #10 start = 0;
+        #60 start = 1;
+        #10 start = 0;
+        #60 start = 1;
+        #10 start = 0;
+        #60 start = 1;
+        #10 start = 0;
+        #60 start = 1;
+        #10 start = 0;
+        #60 start = 1;
+        #10 start = 0;
+        #60 start = 1;
+        #10 start = 0;
     end
 endmodule
