60b2e248c2d21dbfff41f9d0dbba450da7744512
Roger Fan <sweric2u@gmail.com>
2024-11-18 23:28:16 +0800

    Roger: Lab6 Slot Machine

--- roger/Lab6/DEMO/demo1/clock_divisor.v
+++ roger/Lab6/DEMO/demo1/clock_divisor.v
@@ -1,15 +1,12 @@
 module clock_divisor(clk1, clk);
-input clk;
-output clk1;
+    input clk;
+    output clk1;
 
-reg [1:0] num;
-wire [1:0] next_num;
+    reg [1:0] num;
+    wire [1:0] next_num;
 
-always @(posedge clk) begin
-  num <= next_num;
-end
+    always @(posedge clk) num <= next_num;
 
-assign next_num = num + 1'b1;
-assign clk1 = num[1];
-
+    assign next_num = num + 1'b1;
+    assign clk1 = num[1];
 endmodule
